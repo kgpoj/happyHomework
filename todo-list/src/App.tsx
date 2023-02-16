07927565f72451ef1ffd75837f61b29feefe169c
zty <15010972518@163.com>
2023-02-16 12:48:36 +0800

    feat: store todo data in sessionStorage for persistence

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,14 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TodoList from "./components/TodoList";
 import mockTodoData from "./constants/mockTodoData";
 import {TodoItem} from "./interface/TodoItem";
 
+if (!sessionStorage.getItem('mockTodoData')) {
+    sessionStorage.setItem('mockTodoData', JSON.stringify(mockTodoData))
+}
+
 function App() {
     const [dataSource, setDataSource] = useState<TodoItem[]>(mockTodoData);
+    useEffect(() => {
+        setDataSource(JSON.parse(sessionStorage.getItem('mockTodoData') as string))
+    }, []);
 
+    const handleUpdate = (newData: TodoItem[]) => {
+        setDataSource(newData)
+        sessionStorage.setItem('mockTodoData', JSON.stringify(newData))
+    };
     return (
         <div>
-            <TodoList title={'hello'} dataSource={dataSource} onUpdate={setDataSource}/>
+            <TodoList title={'hello'} dataSource={dataSource} onUpdate={handleUpdate}/>
         </div>
     );
 }
