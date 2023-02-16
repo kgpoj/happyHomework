d04709f9c7696ec2bb614c4d436901fca913e1ea
zty <15010972518@163.com>
2023-02-16 12:24:13 +0800

    refactor: directly query for checkbox in test

--- todo-list/src/components/TodoList.test.tsx
+++ todo-list/src/components/TodoList.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {getByText, render, screen} from '@testing-library/react';
 import TodoList from "./TodoList";
 import mockTodoData from "../constants/mockTodoData";
 import userEvent from "@testing-library/user-event";
@@ -9,8 +9,8 @@ describe('TodoList', () => {
     it('should render data and checked status correctly', () => {
         const title = 'hello';
         render(<TodoList title={title} dataSource={mockTodoData} onUpdate={jest.fn}/>)
-        const list = screen.getByRole('list');
-        const checkboxes = Array.from(list.childNodes).map((listItem => listItem.firstChild)) as HTMLInputElement[]
+        const checkboxes = screen.getAllByRole('checkbox') as HTMLInputElement[]
+
         expect(screen.getByText(title)).toBeInTheDocument();
         mockTodoData.forEach((item, index) => {
             const checked = item.status === 'completed'
@@ -25,12 +25,21 @@ describe('TodoList', () => {
             dataSource = newData
         }
         const {rerender} = render(<TodoList title={'hello'} dataSource={dataSource} onUpdate={setDataSource}/>);
-        let list = screen.getByRole('list');
-        let checkbox = list.firstChild!.firstChild as HTMLInputElement
+        let checkbox = screen.getAllByRole('checkbox')[0] as HTMLInputElement
         expect(checkbox).not.toBeChecked()
 
         userEvent.click(checkbox)
         rerender(<TodoList title={'hello'} dataSource={dataSource} onUpdate={setDataSource}/>)
         expect(checkbox).toBeChecked()
     });
+
+    it('should not rerender(for test)', function () {
+        let title = 'title1'
+        render(<TodoList title={title} dataSource={mockTodoData} onUpdate={jest.fn}/>)
+        expect(screen.getByText('title1')).toBeInTheDocument()
+        title = 'title2'
+        expect(screen.getByText('title1')).toBeInTheDocument()
+        expect(screen.queryByText('title2')).not.toBeInTheDocument()
+    });
+
 })
